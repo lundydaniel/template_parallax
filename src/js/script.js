cd9974b36d4fa5586fad7281e801a9698d5d53f7
Daniel Lundy <daniel.lundy@dcmail.ca>
2023-02-07 19:33:57 -0400

    Very basic set up complete. Starting on wireframe.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
 console.log("connected");
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+
+function getScreenSize() {
+    console.log("Browser Width: " + w + ", Height: " + h + ".");
+}
+
+getScreenSize();
